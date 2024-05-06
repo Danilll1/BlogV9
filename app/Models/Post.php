fd959fb4f91f15e6f617ee52ae73cad64a1e072b
Danilll1 <gabrick_72@mail.ru>
2024-05-06 11:24:06 +0500

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+class Post extends Model
+{
+    use Sluggable;
+
+    protected $fillable = ['title', 'description', 'content', 'category_id', 'thumbnail'];
+
+
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class)->withTimestamps();
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function sluggable():array
+    {
+        return [
+            'slug' => [
+                'source'=>'title'
+            ]
+        ];
+    }
+
+    
+}
