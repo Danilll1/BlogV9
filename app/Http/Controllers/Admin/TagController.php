e2ab0e11e19de5275e199c3dd95d4ff26e17e087
Danilll1 <gabrick_72@mail.ru>
2024-05-29 21:23:16 +0500

    tenth commit 16 лаба

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -40,9 +40,11 @@ class TagController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($slug)
     {
-        
+        $tag = Tag::where('slug', $slug)->firstOrFail();
+        $posts = $tag->posts()->orderBy('id', 'desc')->paginate(2);
+        return view('tags.show', compact('tag', 'posts'));
     }
 
     /**
